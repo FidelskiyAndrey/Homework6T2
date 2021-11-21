e93aaf6c9c3e91947a19c6158e46c8b4c52ca82e
FidelskiyAndrey <fidelskiy@gmail.com>
2021-11-21 20:28:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+            text = text.Replace(" ", "");
+            Char[] poli = text.ToCharArray();
+            Array.Reverse(poli);
+            string start = new string(poli);
+            if(text==start)
+            {
+                Console.WriteLine("Является Палиндромом");
+
+            }
+            if (text != start)
+            {
+                Console.WriteLine("Не является Палиндромом");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
